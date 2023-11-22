f6fb6b645ce094c777f51382ff32b106aeb44d36
Leotaxx <lyleride@gmail.com>
2023-11-22 15:45:15 +0000

    remove function part

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -5,24 +5,24 @@ import { useNavigate } from 'react-router-dom';
 const LoginPage=()=>{
     const[email,setEmail]=useState('');
     const[password,setPassword]=useState('');
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    const handleSubmit = async (event)=>{
-        event.preventDefault();
-        try{
-            const response = await axios.post('mongodb+srv://lyleride:lyleride@cluster0.al8raba.mongodb.net/?retryWrites=true&w=majority'
-            ,{email,password});
-            console.log(response.data);
-            navigate('/profile');
-        }catch(error){
-            console.error(error);
-        }
-    };
+    // const handleSubmit = async (event)=>{
+    //     event.preventDefault();
+    //     try{
+    //         const response = await axios.post('mongodb+srv://lyleride:lyleride@cluster0.al8raba.mongodb.net/?retryWrites=true&w=majority'
+    //         ,{email,password});
+    //         console.log(response.data);
+    //         navigate('/profile');
+    //     }catch(error){
+    //         console.error(error);
+    //     }
+    // };
 
 
     return(
         
-        <form onSubmit={handleSubmit}>
+        <form >
             <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} required/>
             <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} required/>
             <button type='submit'>Login</button>
